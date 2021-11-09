5a576d92e6445341c8aa472603d5e5c2068efdd3
Noy <noy@KezurersiPadPro.meetinplace>
2021-11-09 15:40:24 +0200

    cleaned the code

--- simple-server.js
+++ simple-server.js
@@ -9,8 +9,8 @@ fileToContentType.set('css', 'text/css');
 fileToContentType.set('html', 'text/html');
 fileToContentType.set('json', 'application/json');
 
-function handleFile(data, ending, res) {
-    res.writeHead(200, {'Content-Type': fileToContentType.get(ending)});
+function handleFile(data, extention, res) {
+    res.writeHead(200, {'Content-Type': fileToContentType.get(extention)});
     res.write(data);
     res.end();
 }
@@ -19,7 +19,6 @@ console.log(__filename, module, global)
 
 http.createServer(function (req, res) {
     const q = url.parse(req.url);
-    //let ending = req.url.split('.').pop();
     let extention = q.pathname.split('.')[1];    
     fs.readFile('public' + q.pathname, function(err, data) {
         if(data) {
