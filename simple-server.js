0d10111336c71ef005e84e69787ca6abf6b775ab
Noy <noy@Noys-MacBook-Pro.local>
2021-11-15 08:55:54 +0200

    update server to include all possible filters

--- simple-server.js
+++ simple-server.js
@@ -1,94 +1,81 @@
-const FISRT_QUERY = 0;
-const SECOND_QUERY = 1;
-
 const http = require('http');
 const fs = require('fs');
 const url = require('url');
 
-const fileToContentType = new Map();
-fileToContentType.set('js', 'text/javascript');
-fileToContentType.set('css', 'text/css');
-fileToContentType.set('html', 'text/html');
-fileToContentType.set('json', 'application/json');
+const fileExtentionToContentType = {'js': 'text/javascript', 
+                                    'css': 'text/css', 
+                                    'html': 'text/html',
+                                    'json': 'application/json'}
+
+const filters = ['from', 'by'];
+let filterToValueMap;
 
 
-// TODO: For improvment: Use directly filterKey 
 function filterJsonFile(data, filterKey, filterValue) {
-    if(filterKey === "by") {
-        return JSON.parse(data).filter(function(entry) {
-            return entry.by.toLowerCase() === filterValue.toLowerCase();
-        });
-    }
-    else if(filterKey === "from") {
-        return JSON.parse(data).filter(function(entry) {
-            return entry.from.toLowerCase() === filterValue.toLowerCase();
-        });
-    }
+    return JSON.stringify(JSON.parse(data).filter(function(entry) {
+        return entry[filterKey].toLowerCase() === filterValue.toLowerCase();
+    }));
 }
 
 
-function updateFiltersKeyAndValue(query, filtersKey, filtersValue, queryIndex) {
-    let KeyAndValue = query.split('=');
-    filtersKey[queryIndex] = KeyAndValue[0];
-    filtersValue[queryIndex] = KeyAndValue[1].replace('_',' ');
+function updateFiltersValue(query) {
+    let splitedQuery = query.split('&');
+    splitedQuery.forEach(function(q) {
+        let KeyAndValue = q.split('=');
+        filterToValueMap.set(KeyAndValue[0], KeyAndValue[1].replace('_', ' '));
+    } );
 }
 
 
-function parseQueriesAndUpdateFilters(query, filtersKey, filtersValue) {
-    let splitedQueries = query.split('&');
-    if(splitedQueries[FISRT_QUERY]) {
-        updateFiltersKeyAndValue(splitedQueries[FISRT_QUERY], filtersKey, filtersValue, FISRT_QUERY);
-        if(splitedQueries[SECOND_QUERY]) {
-            updateFiltersKeyAndValue(splitedQueries[SECOND_QUERY], filtersKey, filtersValue, SECOND_QUERY);
-        } 
-    }
-}
-
-
-function getDataFromJsonFile(query, dataToWrite){
-    if(query) {
-        let filtersKey = ['', ''];
-        let filtersValue = ['', ''];
-        parseQueriesAndUpdateFilters(query, filtersKey, filtersValue);
-        if(filtersKey[FISRT_QUERY] !== '') {
-            dataToWrite = JSON.stringify(filterJsonFile(dataToWrite, filtersKey[FISRT_QUERY], filtersValue[FISRT_QUERY]));
-            if(filtersKey[SECOND_QUERY] !== '') {
-                dataToWrite = JSON.stringify(filterJsonFile(dataToWrite, filtersKey[SECOND_QUERY], filtersValue[SECOND_QUERY]));
-            }
+function getFilteredDataFromJson(query, dataToWrite){
+    updateFiltersValue(query);
+    filterToValueMap.forEach(function(value, key) {
+        if(value !== '') {
+            dataToWrite = filterJsonFile(dataToWrite, key, value);
         }
-    }
+    });
     return dataToWrite;
 }
 
 
-function handleFile(res, data, query, extention) {
+function sendFileContentToClient(response, data, query, fileExtention) {
     let dataToWrite = data;
-    if(extention === 'json') {
-        dataToWrite = getDataFromJsonFile(query, dataToWrite);
+    if(fileExtention === 'json' && query) {
+        dataToWrite = getFilteredDataFromJson(query, dataToWrite);
     }
-    res.writeHead(200, {'Content-Type': fileToContentType.get(extention)});
-    res.write(dataToWrite);
-    res.end();
+    response.writeHead(200, {'Content-Type': fileExtentionToContentType[fileExtention]});
+    response.write(dataToWrite);
+    response.end();
 }
 
 
-console.log(__filename, module, global);
+function handleFileNotFound(response) {
+    response.writeHead(404, {'Content-Type': 'text/plain'});
+    response.end('Not found');
+}
 
 
-http.createServer(function (req, res) {
-    const parsedUrl = url.parse(req.url);
-    const extention = parsedUrl.pathname.split('.')[1]; 
-    const query = parsedUrl.query;   
-    
-    fs.readFile('public' + parsedUrl.pathname, function(err, data) {
+function handleFile(response, pathname, query, fileExtention) {
+    fs.readFile('public' + pathname, function(err, data) {
         if(data) {
-            handleFile(res, data, query, extention);
+            sendFileContentToClient(response, data, query, fileExtention);
         }
         else {
-            res.writeHead(404, {'Content-Type': 'text/plain'});
-            res.end('Not found');
+            handleFileNotFound(response);
         }
     })
+}
+
+
+http.createServer(function(request, response) {
+    filterToValueMap = new Map();
+    filters.forEach(filter => filterToValueMap.set(filter, ''));
+
+    const parsedUrl = url.parse(request.url);
+    const fileExtention = parsedUrl.pathname.split('.')[1]; 
+    
+    handleFile(response, parsedUrl.pathname, parsedUrl.query, fileExtention);
+
 }).listen(8080, function() {
     console.log('Client is available at http://localhost:8080');
 });
